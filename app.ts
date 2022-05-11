5a09d17e636f12c1c8335f2dd2ec156e6d6ed51f
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2022-05-10 22:12:25 -0300

    3: number, string, booleans

--- app.ts
+++ app.ts
@@ -1,9 +1,17 @@
-function add(number1: number, number2: number) {
-  return number1 + number2
+function add(
+  number1: number,
+  number2: number,
+  printResult: boolean,
+  resultPhrase: string
+) {
+  const result = number1 + number2
+  if (printResult) console.log(resultPhrase + result)
+  return result
 }
 
 const number1 = 5
 const number2 = 2.8
+const printResult = true
+const resultPhrase = 'Result is: '
 
-const result = add(number1, number2)
-console.log(result)
+add(number1, number2, printResult, resultPhrase)
