d07d47fa60a8397d6cf199397e6fba06702f53ef
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2022-05-10 22:49:41 -0300

    4; type inference and type assignment

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-function add(number1, number2) {
-  return number1 + number2
+function add(number1, number2, printResult, resultPhrase) {
+    var result = number1 + number2;
+    if (printResult)
+        console.log(resultPhrase + result);
+    return result;
 }
-var number1 = 5
-var number2 = 2.8
-var result = add(number1, number2)
-console.log(result)
+var number1 = 5; //type = 5 (inference)
+var number2 = 2.8; //type = number (inference)
+var number3; //type = number (assignment)
+number3 = 1;
+var printResult = true;
+var resultPhrase = 'Result is: ';
+// resultPhrase = 1 //error => infered type is string, not number
+add(number1, number2, printResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,17 +1,15 @@
-function add(
-  number1: number,
-  number2: number,
-  printResult: boolean,
-  resultPhrase: string
-) {
+function add(number1: number, number2: number, printResult: boolean, resultPhrase: string) {
   const result = number1 + number2
   if (printResult) console.log(resultPhrase + result)
   return result
 }
 
-const number1 = 5
-const number2 = 2.8
+const number1 = 5 //type = 5 (inference)
+let number2 = 2.8 //type = number (inference)
+let number3: number //type = number (assignment)
+number3 = 1
 const printResult = true
-const resultPhrase = 'Result is: '
+let resultPhrase = 'Result is: '
+// resultPhrase = 1 //error => infered type is string, not number
 
 add(number1, number2, printResult, resultPhrase)
